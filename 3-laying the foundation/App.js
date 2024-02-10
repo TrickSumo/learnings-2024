0101498813ec6bf87611011e81c38c716e260292
Rishi Kumar Tiwari <rishi008.tiwari@gmail.com>
2024-02-10 16:21:30 +0530

    first react papp - restuarant

--- 3-laying the foundation/App.js
+++ 3-laying the foundation/App.js
@@ -22,13 +22,90 @@ const jsxEle2 = (
 );
 
 // React Functional component
-
 const HeadingComponent = () => {
   return <h1 id="heading">Simple string 🚀 React Functional component</h1>;
 };
 
-const HeadingComponent2 = () => (
-  <h1 id="heading">Simple string 🚀 React Functional component2</h1>
+const num = 9000;
+
+//Component Composition => one functional component is used in another
+const Title = () => {
+  return <h1 id="heading">Simple string 🚀 React Functional component2</h1>;
+};
+const HeadingComponent2 = () => {
+  return (
+    <div>
+      <Title />
+      <div>Paragraph here</div>
+      Power level over {num}
+    </div>
+  );
+};
+
+root.render(<HeadingComponent2 />);
+
+//Putting react element insdie react functionlal component
+const title = <h1 id="heading">Simple string 🚀 React element</h1>;
+
+const HeadingComponent3 = () => {
+  return (
+    <div>
+      {title}
+      <div>Paragraph here</div>
+      Power level over {num}
+    </div>
+  );
+};
+
+root.render(<HeadingComponent3 />);
+
+//infinite loop
+
+// const HeadingComponent4 = () => {
+//   return (
+//     <div>
+//       {title4}
+//       <div>Paragraph here</div>
+//       Power level over {num}
+//     </div>
+//   );
+// };
+// const title4 = (
+//   <h1 id="heading">Simple string 🚀 React element {<HeadingComponent4 />}</h1>
+// );
+
+// root.render(<HeadingComponent4 />);
+
+// functional component inside react element
+
+const HeadingComponent4 = () => {
+  return (
+    <div>
+      <div>Paragraph here</div>
+      Power level over {num}
+    </div>
+  );
+};
+const title4 = (
+  <div id="heading">Simple string 🚀 React element {<HeadingComponent4 />}</div>
 );
 
-root.render(<HeadingComponent />);
+root.render(title4);
+
+//Three ways to call functional component
+
+const Title6 = () => {
+  return <h1 id="heading">Simple string 🚀 React Functional component2</h1>;
+};
+const HeadingComponent6 = () => {
+  return (
+    <div>
+      <Title6 />
+      <Title6></Title6>
+      {Title6()}
+      <div>Paragraph here</div>
+      Power level over {num}
+    </div>
+  );
+};
+root.render(<HeadingComponent6 />);
