4dc9c43ca408bc3b6eebda3f9d1577c183b51277
Rishi Kumar Tiwari <rishi008.tiwari@gmail.com>
2024-02-17 17:32:50 +0530

    search and filter

--- 6-expolring-the-world/src/components/Body.js
+++ 6-expolring-the-world/src/components/Body.js
@@ -4,7 +4,9 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
+  const [filteredResData, setFilteredResData] = useState([]);
   const [resData, setResData] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState("");
 
   useEffect(() => {
     console.log("Use Effect called");
@@ -23,22 +25,49 @@ const Body = () => {
     setResData(
       res?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+    setFilteredResData(
+      res?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  const handleSearch = () => {
+    // if (searchKeyword.length > 0) {
+    console.log(
+      "after search",
+      resData.filter((res) =>
+        res?.info?.name?.toLowerCase().includes(searchKeyword.toLowerCase())
+      )
+    );
+    setFilteredResData(() =>
+      resData.filter((res) =>
+        res?.info?.name?.toLowerCase().includes(searchKeyword.toLowerCase())
+      )
+    );
+    // }
   };
 
   return resData?.length === 0 ? (
-    <Shimmer />
+    <>
+      {/* <Shimmer /> <button onClick={() => console.log(resData)}>Console Log State</button> */}
+    </>
   ) : (
     <div className="body">
+      {/* <button onClick={() => console.log(filteredResData)}>Console Log State</button> */}
       <div className="filter" key="filter">
         <div className="search">
-          <input type="text" className="search-box" />
-          <button>Search</button>
+          <input
+            type="text"
+            className="search-box"
+            value={searchKeyword}
+            onChange={(e) => setSearchKeyword(e.target.value)}
+          />
+          <button onClick={() => handleSearch()}>Search</button>
         </div>
         <button
           className="filter-btn"
           onClick={() => {
-            setResData((curr) =>
-              curr.filter((res) => res.info.avgRating > 4.2)
+            setFilteredResData(() =>
+              resData.filter((res) => res.info.avgRating > 4.2)
             );
           }}
         >
@@ -46,17 +75,19 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container" k="res">
-        {resData.map((res) => (
-          <RestuarantCard
-            key={res?.id}
-            resName={res?.info?.name}
-            cuisine={res?.info?.cuisines?.join(", ")}
-            stars={res?.info?.avgRating}
-            time={res?.info?.sla?.deliveryTime}
-            imageURL={cloudinaryURL + res?.info?.cloudinaryImageId}
-            costForTwo={res?.info?.costForTwo}
-          />
-        ))}
+        {filteredResData.length > 0
+          ? filteredResData.map((res) => (
+              <RestuarantCard
+                key={res?.id}
+                resName={res?.info?.name}
+                cuisine={res?.info?.cuisines?.join(", ")}
+                stars={res?.info?.avgRating}
+                time={res?.info?.sla?.deliveryTime}
+                imageURL={cloudinaryURL + res?.info?.cloudinaryImageId}
+                costForTwo={res?.info?.costForTwo}
+              />
+            ))
+          : "No Restuarant Found!"}
       </div>
     </div>
   );
